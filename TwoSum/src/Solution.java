1017e625d4c8ddb9ed0b1b685a06efb39d6af9c9
Roman Akc <sporco1988@gmail.com>
2022-11-01 14:10:23 +0600

    added new decision

--- TwoSum/src/Solution.java
+++ TwoSum/src/Solution.java
@@ -1,5 +1,22 @@
+import java.util.HashMap;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        var map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; ++i) {
+            map.put(nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length; ++i) {
+            var val = target - nums[i];
+            if(map.containsKey(val) && map.get(val) != i)
+                return new int[] {i, map.get(val)};
+        }
+
+        return null;
+    }
+
+    public int[] twoSum_old(int[] nums, int target) {
         int result [] = new int[2];
         for(int i = 0; i < nums.length; ++i) {
             for(int j = i + 1; j < nums.length; ++j) {

a7b88424751e52bfbd3e0daaef36fc290d772260
Roman Akc <sporco1988@gmail.com>
2022-11-17 15:12:57 +0600

    FindAllAnagramsInAString

---
+++ FindAllAnagramsInAString/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.findAnagrams("cbaebabacd", "abc"));
+        System.out.println(s.findAnagrams("abab", "ab"));
+    }
+}
---
+++ FindAllAnagramsInAString/src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        HashMap<String, Integer>
+        int k = p.length();
+        for(int i = k; i <= s.length(); ++i) {
+
+        }
+
+        return list;
+    }
+}

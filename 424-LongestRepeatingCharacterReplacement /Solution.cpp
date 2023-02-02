5547125e87848673e5548e96036e9adf25116895
Roman Akc <sporco1988@gmail.com>
2023-02-02 22:39:19 +0600

    added LongestRepeatingCharacterReplacement frame

---
+++ 424-LongestRepeatingCharacterReplacement /Solution.cpp
@@ -0,0 +1,9 @@
+//
+// Created by roman on 02.02.23.
+//
+
+#include "Solution.h"
+
+int Solution::characterReplacement(std::string s, int k) {
+
+}
---
+++ 424-LongestRepeatingCharacterReplacement /Solution.h
@@ -0,0 +1,10 @@
+//
+// Created by roman on 02.02.23.
+//
+#pragma once
+#include <string>
+
+class Solution {
+    int characterReplacement(std::string s, int k);
+};
+
